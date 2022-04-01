8f6a5c3cb273fdaec005383eaa093e7a4cbe02bc
Aziza Khamzayeva <azizakhamzayeva.02@gmail.com>
2022-04-01 14:42:17 +0600

    last

--- movies/forms.py
+++ movies/forms.py
@@ -1,2 +1,9 @@
-class ReviewForm:
-    pass
+from django import forms
+
+from .models import Reviews
+
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Reviews
+        fields = ("name", "email", "text")
--- movies/views.py
+++ movies/views.py
@@ -1,20 +1,30 @@
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
+
 from .models import Movie
 from .forms import ReviewForm
 
 
 class MoviesView(ListView):
-    # spisok filmov
 
     model = Movie
     queryset = Movie.objects.filter(draft=False)
-    template_name = "movies/movies.html"
 
 
 class MovieDetailView(DetailView):
-    # polnoe opisanie film
 
     model = Movie
     slug_field = "url"
+
+
+class AddReview(View):
+
+    def post(self, request, pk):
+        form = ReviewForm(request.POST)
+        movie = Movie.objects.get(id=pk)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.movie = movie
+            form.save()
+        return redirect(movie.get_absolute_url())
